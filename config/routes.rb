1d972f0313867b05759e7816dc79707c66dcea22
jnel1234 <johnpaulnelson18@gmail.com>
2021-03-04 15:31:09 -0800

    authenticated and unauth accounts have different roots

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   
   resources :friends
   get 'home/about'
-  root 'friends#index'
+ 
+  devise_scope :user do
+
+    unauthenticated :user do
+      root :to => "devise/sessions#new"
+    end
+    authenticated :user do
+      root :to => 'friends#index', as: :authenticated
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
