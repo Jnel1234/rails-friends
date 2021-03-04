d7124ca7853a6df1a0c94746889717c34c537b4e
jnel1234 <johnpaulnelson18@gmail.com>
2021-03-04 14:18:39 -0800

    changed home page again

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   get 'home/about'
-  root 'registration#new'
+  root 'registrations#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
